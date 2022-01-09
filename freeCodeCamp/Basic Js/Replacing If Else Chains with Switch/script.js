32470ab81dac44d45cad596bf4feb7b4b4be92dd
”Serj” <serjvertelko@gmail.com>
2022-01-09 21:38:38 +0300

    feat: comlited task

---
+++ freeCodeCamp/Basic Js/Replacing If Else Chains with Switch/script.js
@@ -0,0 +1,31 @@
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+
+    switch (val) {
+        case "bob":
+            return "Marley";
+            breake;
+
+        case 42:
+            return "The Answer";
+            breake;
+
+        case 1:
+            return "There is no #1"
+            breake;
+
+        case 99:
+            return "Missed me by this much!";
+            breake;
+
+        case 7:
+
+            return "Ate Nine";
+            breake;
+    }
+    // Only change code above this line
+    return answer;
+}
+
+chainToSwitch(7);
