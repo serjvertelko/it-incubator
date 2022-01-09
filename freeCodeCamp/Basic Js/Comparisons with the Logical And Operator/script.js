27ef4f0fc562038c7df1fff60e2654fec6f80df0
”Serj” <serjvertelko@gmail.com>
2022-01-09 19:28:26 +0300

    feat: comlited task

---
+++ freeCodeCamp/Basic Js/Comparisons with the Logical And Operator/script.js
@@ -0,0 +1,10 @@
+function testLogicalAnd(val) {
+    // Only change code below this line
+    if (val <= 50 && val >= 25) {
+        return "Yes";
+    } else {
+        // Only change code above this line
+        return "No";
+    }
+}
+testLogicalAnd(10);
