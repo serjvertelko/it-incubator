717b3602dfd1e4cf6b18d70fb61a32e7528ba1ef
”Serj” <serjvertelko@gmail.com>
2022-01-09 22:24:07 +0300

    feat: comlited task

---
+++ freeCodeCamp/Basic Js/Build JavaScript Objects/script.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    name: "Chak",
+    legs: 4,
+    tails: 1,
+    friends: ["Jack", "Sandy"]
+        // Only change code above this line
+};
