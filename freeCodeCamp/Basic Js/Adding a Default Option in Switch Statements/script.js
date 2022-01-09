36c05a35f0e4706a0bb6db6b36c793b5a45011b5
”Serj” <serjvertelko@gmail.com>
2022-01-09 21:17:25 +0300

    feat: comlited task

---
+++ freeCodeCamp/Basic Js/Adding a Default Option in Switch Statements/script.js
@@ -0,0 +1,26 @@
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case "a":
+            return "apple";
+            break;
+
+        case "b":
+            return "bird";
+            breake;
+
+        case "c":
+            return "cat";
+            breake;
+
+        default:
+            return "stuff";
+            breake
+
+    }
+    // Only change code above this line
+    return answer;
+}
+
+switchOfStuff(1);
