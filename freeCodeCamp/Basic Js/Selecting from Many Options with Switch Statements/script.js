0168e842c5e2d68996e1942614a2d7e59bfe6cfe
”Serj” <serjvertelko@gmail.com>
2022-01-09 21:12:51 +0300

    feat: comlited task

---
+++ freeCodeCamp/Basic Js/Selecting from Many Options with Switch Statements/script.js
@@ -0,0 +1,25 @@
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+            return ("alpha");
+            breake;
+
+        case 2:
+            return ("beta");
+            breake;
+
+        case 3:
+            return ("gamma");
+            breake;
+
+        case 4:
+            return ("delta");
+            breake
+    }
+    // Only change code above this line
+    return answer;
+}
+
+caseInSwitch(1);
