c6e688035f1f98cab7147b5c8701df1a8f119df6
”Serj” <serjvertelko@gmail.com>
2022-01-09 21:21:39 +0300

    feat: comlited task

---
+++ freeCodeCamp/Basic Js/Multiple Identical Options in Switch Statements/script.js
@@ -0,0 +1,24 @@
+function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+        case 2:
+        case 3:
+            return "Low";
+            breake;
+        case 4:
+        case 5:
+        case 6:
+            return "Mid";
+            breake;
+        case 7:
+        case 8:
+        case 9:
+            return "High";
+            breake;
+    }
+    // Only change code above this line
+    return answer;
+}
+sequentialSizes(1);
