c1e79e52b612502207308a6533432c850f7975ca
”Serj” <serjvertelko@gmail.com>
2022-01-09 23:09:34 +0300

    feat: comlited task

---
+++ freeCodeCamp/Basic Js/Manipulating Complex Objects/script.js
@@ -0,0 +1,23 @@
+const myMusic = [{
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+        "CD",
+        "8T",
+        "LP"
+    ],
+    "gold": true
+}];
+
+myMusic.unshift({
+    "artist": "Adam Smidt",
+    "title": "Piano Man",
+    "release_year": 1983,
+    "formats": [
+        "CD",
+        "8T",
+        "LP"
+    ],
+    "gold": true
+})
